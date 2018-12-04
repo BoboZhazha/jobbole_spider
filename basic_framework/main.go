0dd711c1e9b337502051253a4747c06924707e1e
zhang@mac <909319019@qq.com>
2018-12-04 09:25:16 +0800

    单任务版修改种子数量测试

--- basic_framework/main.go
+++ basic_framework/main.go
@@ -3,19 +3,35 @@ package main
 import (
 	"jobbole_spider/basic_framework/engine"
 	"jobbole_spider/basic_framework/jobbole/parser"
+	"strconv"
 )
 
 func main() {
-	engine.Run(engine.Request{
-		Url:        "http://blog.jobbole.com/all-posts",
-		ParserFunc: parser.ParseArticleList,
-	}, engine.Request{
-		Url:        "http://blog.jobbole.com/all-posts/page/2",
-		ParserFunc: parser.ParseArticleList,
-	}, engine.Request{
-		Url:        "http://blog.jobbole.com/all-posts/page/3",
-		ParserFunc: parser.ParseArticleList,
-	},
-	)
+
+	const baseUrl = "http://blog.jobbole.com/all-posts"
+	var seeds []engine.Request
+
+	for i := 1; i < 500; i++ {
+		urlStr := baseUrl + "/page/" + strconv.FormatInt(int64(i), 10)
+		request := engine.Request{
+			Url:        urlStr,
+			ParserFunc: parser.ParseArticleList,
+		}
+		seeds = append(seeds, request)
+	}
+
+	//engine.Run(engine.Request{
+	//		Url:        "http://blog.jobbole.com/all-posts",
+	//		ParserFunc: parser.ParseArticleList,
+	//	}, engine.Request{
+	//		Url:        "http://blog.jobbole.com/all-posts/page/2",
+	//		ParserFunc: parser.ParseArticleList,
+	//	}, engine.Request{
+	//		Url:        "http://blog.jobbole.com/all-posts/page/3",
+	//		ParserFunc: parser.ParseArticleList,
+	//	},
+	//)
+
+	engine.Run(seeds...)
 
 }
